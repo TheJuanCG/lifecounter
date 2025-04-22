9b2925a16fffd5df0525c48649eb39ddafef8e77
TheJuanCG <114948315+TheJuanCG@users.noreply.github.com>
2025-04-21 20:19:35 -0700

    Finished most homework

---
+++ LifeCounter/LifeCounter/LifeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCounterApp.swift
+//  LifeCounter
+//
+//  Created by Juan Carlos Garcia on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
